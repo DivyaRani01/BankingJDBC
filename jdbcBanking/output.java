c6e79fbad22ab3c4f76ead181fc8056f702b9c8f
DivyaRani01 <divya04200@gmail.com>
2022-09-08 23:11:28 +0530

    jdbc changes

---
+++ jdbcBanking/output.java
@@ -0,0 +1,56 @@
+					||>------------------WELCOME TO BANK MANAGEMENT SYSTEM----------------------------<||
+
+1 - To Create a Bank Account 
+2 - to login to existing Account 
+3 - Exit
+
+Enter Your choice : 1
+
+Enter User Name : Mercy
+Set Account Password : 0987
+Enter Phone No : 8810188664
+Enter Age : 54
+Enter Account Balance : 40000
+
+ Your Account Created Successfully
+1 rows affected.
+					||>------------------WELCOME TO BANK MANAGEMENT SYSTEM----------------------------<||
+
+1 - To Create a Bank Account 
+2 - to login to existing Account 
+3 - Exit
+
+Enter Your choice : 2
+
+Enter Account Holder Name : Charlie
+Enter Account Password : 123456
+
+********Login Success******!
+			....................Enter your Choice......................... 
+
+1 - ***To Print Account Detail 
+2 - ***To Deposit Amount 
+3 - ***To Withdraw Amount 
+4 - ***To Change Your Pin 
+5 - ***To Log Out.
+
+ Press :  2
+
+Enter Amount to Deposit : 2000
+
+***Updated Account Balance : 7000
+			....................Enter your Choice......................... 
+
+1 - ***To Print Account Detail 
+2 - ***To Deposit Amount 
+3 - ***To Withdraw Amount 
+4 - ***To Change Your Pin 
+5 - ***To Log Out.
+
+ Press :  4
+Enter Account Holder Name : Charlie
+Enter Current Password : 123456
+Enter New Password : 56372
+
+PassWord Successfully Changed. 
+Login Again with New Password!
